dc4c6743a9579b4a2407a5f0f4e78da4ab2b59db
Rajesh <34510099+rajeshramesh38@users.noreply.github.com>
2019-09-08 21:46:14 +0530

    Update apriori.py

--- experiment_01/apriori.py
+++ experiment_01/apriori.py
@@ -1,3 +1,12 @@
-import numpy as np
-import pandas as pd
 from apyori import apriori
+import pandas as pd
+import numpy as np
+
+store_data = pd.read_csv("../datasets/apiori.csv", header=None)
+print(store_data)
+records = list()
+for i in range(0, 22):
+    records.append([str(store_data.values[i, j]) for j in range(0, 6)])
+association_rules = apriori(records, min_support=0.50, min_confidence=0.7, min_lift=1.2, min_length=2)
+association_results = list(association_rules)
+print(association_results)
